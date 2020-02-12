952965e747423b7c08ff530a8484e5ece25552a8
Abdul Khan <akhan17@outlook.com>
2020-02-12 19:10:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(string)
+  array02 = []
+  array01 = string.split
+  #puts array01
 
+  count = 0 
+  while count != array01.length
+  array02.push(array01[count].reverse)
+  count +=1
+end
+puts array02.join(' ')
 end
