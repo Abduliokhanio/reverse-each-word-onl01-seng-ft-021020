5d197ea359db2b0f4537806677a57862621c2dc1
Abdul Khan <akhan17@outlook.com>
2020-02-12 19:19:47 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  array2 = []
+  array01 = string.split
+  #puts array01
+
+  array01.collect do |letters|
+    array2.push(letters.reverse)
+  end
+  return array2.join(" ")
+end
